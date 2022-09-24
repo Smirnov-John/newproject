35ae777ef3ccea578c3e20747e9779f06085c600
Smirnov-John <smirnovevvi@mail.ru>
2022-09-24 13:07:50 +0300

    Изменения в проекте

--- JS1/index.js
+++ JS1/index.js
@@ -1,5 +1,5 @@
-console.log("Hello world!")
-const ourPlanetName = "Земля";
+//console.log("Hello world!");
+/*const ourPlanetName = "Земля";
 console.log(ourPlanetName);
 let userTime = 12;
 console.log(userTime);
@@ -10,4 +10,36 @@ console.log(isCach);
 let userName = "Evgeniy";
 let userName1 = "Smirnov";
 let userName2 = "Victorovich";
-console.log(userName1,userName,userName2);
+console.log(userName1, userName, userName2);
+
+const age = 18;
+
+const userData = `Sum: ${1+3}`;
+
+console.log(userData);
+
+const isAdmin = false;
+const isVisible = true;
+console.log(isAdmin);*/
+
+// let townName;
+
+// console.log(townName);
+
+// townName = "Moscow";
+
+// console.log(townName);
+
+// const userData = {
+//   name: "Иван",
+//   age: 18,
+// };
+// console.log(userData.age);
+
+// // alert('Hello world');
+
+// console.log(typeof userData.age);
+
+const num = 127361273578632117212n;
+
+console.log(num);
