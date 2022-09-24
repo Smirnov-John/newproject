2bd32ba5b5d367d756f9db96b3c32b9531285534
Smirnov-John <smirnovevvi@mail.ru>
2022-09-24 10:39:13 +0300

    добавил файлы с первого занятия

---
+++ JS1/index.js
@@ -0,0 +1,13 @@
+console.log("Hello world!")
+const ourPlanetName = "Земля";
+console.log(ourPlanetName);
+let userTime = 12;
+console.log(userTime);
+let nextText = 32;
+console.log(nextText);
+let isCach = true;
+console.log(isCach);
+let userName = "Evgeniy";
+let userName1 = "Smirnov";
+let userName2 = "Victorovich";
+console.log(userName1,userName,userName2);
